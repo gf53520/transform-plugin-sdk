9d6ef542143aa9300e1ef42c64b33074d1e07e91
guifeng <guifengleaf@gmail.com>
2016-11-24 14:38:33 +0800

    modifyPluginOutputsFields

--- src/main/scala/com/qiniu/pipeline/sdk/examples/AppendDateTimeJavaParser.java
+++ src/main/scala/com/qiniu/pipeline/sdk/examples/AppendDateTimeJavaParser.java
@@ -7,11 +7,16 @@ import org.apache.spark.sql.types.StructType;
 
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 
 public class AppendDateTimeJavaParser extends JavaParser {
+
   private String TIMEFORMAT = "MM/dd/yyyy HH:mm:ss";
+  private SimpleDateFormat sdf = new SimpleDateFormat(TIMEFORMAT);
 
   public AppendDateTimeJavaParser(Integer order, List<String> pluginFields, StructType schema, Map<String, Serializable> configuration) {
     super(order, pluginFields, schema, configuration);
@@ -19,20 +24,17 @@ public class AppendDateTimeJavaParser extends JavaParser {
 
   /**
    * @param originData 用户每行打点/输入数据
-   * @return 每行输出数据, 必须由originData与pluginFields中每个字段对应的字段值共同组成, 其中pluginFields为用户Plugin中所写字段
+   * @return 每行输出数据, 必须由pluginFields中每个字段所对应的字段值共同组成, 其中pluginFields为用户创建transform的Plugin中所写字段
    */
   @Override
   public List<Row> parse(Row originData) {
-    SimpleDateFormat sdf = new SimpleDateFormat(TIMEFORMAT);
     String date = sdf.format(new Date());
-    GenericRow genericRow = (GenericRow) originData;
 
-    // 最初list由用户原始输入数据组成
-    List<Object> list = new ArrayList<Object>(Arrays.asList(genericRow.values()));
-
-    // list添加pluginFields中字段对应的字段值
+    // 将pluginFields中所有字段的对应字段值放入List中, 且本例子中pluginFields只有一个字段, 因此List只需存放该字段的对应值
+    List<Object> list = new ArrayList<Object>();
     list.add(date);
-    // 返回一个List, 其中每行必须由原始输入数据与pluginFields中每个字段对应的字段值共同组成
+
+    // 返回一个List, 其中每行数据必须由pluginFields中所有字段对应的字段值共同组成, 且由于本例子是单行到单行的转换, 因此List的size为1.
     List<Row> result = new ArrayList<Row>();
     result.add(new GenericRow(list.toArray()));
     return result;
